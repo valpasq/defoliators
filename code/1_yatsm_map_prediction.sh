99a600661a84ad37dd4d2b6762d6018fb09560b3
Valerie <valpasq@bu.edu>
2017-04-29 09:50:42 -0400

    script for generating synthetic images from CCDC results

---
+++ code/1_yatsm_map_prediction.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#$ -l h_rt=24:00:00
+#$ -V
+#$ -N map_prediction
+#$ -j y
+
+if [ -z "$1" ]; then
+    echo "Error - please specify a directory with extraced Landsat archives. Usage:"
+    echo "    $0 <directory>"
+    exit 1
+fi
+
+# First input is location of image directory
+here=$1
+cd $here
+
+# Date to predict image for
+predictdate=$2
+
+# Specify YATSM results directory
+#run="moths_paper_3"
+run="moths_0414"
+
+# Specify results prefix to use
+prefix="ols"
+
+# GMap predictions
+yatsm -v map --date "%Y-%j" --result ./YATSM_${run}/ --before --qa --refit_prefix $prefix \
+	predict $predictdate ${run}_prediction_${predictdate}_qa.tif
