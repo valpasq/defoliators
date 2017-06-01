c32da81f0536514ef041acd36363a2d9e2bae85f
Valerie <valpasq@bu.edu>
2017-06-01 10:52:13 -0400

    updated script to use only TCG, 2016 data

--- code/2_calculateresiduals.py
+++ code/2_calculateresiduals.py
@@ -4,7 +4,7 @@ from osgeo import gdal, gdal_array
 import pandas as pd
 
 # Read in CSV with dataset locations
-img_list = pd.read_csv('../config/moths_datasets_0515.csv')
+img_list = pd.read_csv('../config/moths_datasets_2016.csv')
 
 for img_id in img_list.index:
 
@@ -19,7 +19,6 @@ for img_id in img_list.index:
 	with rasterio.open(img_obs_fn) as image:
 	    obs_ds = image.read()
 	    obs_G = obs_ds[8] # Greenness
-	    obs_W = obs_ds[9] # Wetness
 	    obs_mask = obs_ds[10] # Fmask result
 
 	# Read in predicted image
@@ -28,27 +27,19 @@ for img_id in img_list.index:
 	with rasterio.open(img_pred_fn) as image:
 	    pred_ds = image.read()
 	    pred_G = pred_ds[8] # Predicted Greenness
-	    pred_W = pred_ds[9] # Predicted Wetness
 
 	# Read in RMSE
 	rmse_G_fn = img_list['rmse_G'][img_id]
-	rmse_W_fn = img_list['rmse_W'][img_id]
 
 	with rasterio.open(rmse_G_fn) as image:
 	    rmse_G_ds = image.read()
 	    rmse_G = rmse_G_ds[4] # RMSE
 
-	with rasterio.open(rmse_W_fn) as image:
-	    rmse_W_ds = image.read()
-	    rmse_W = rmse_W_ds[4] # RMSE
-
 	# Difference: observed - predicted
 	diff_G = (obs_G - pred_G) # subtract
-	diff_W = (obs_W - pred_W) # subtract
 
 	# Stadardize: use CCDC model RMSE
 	diff_G_stand = (obs_G - pred_G) / rmse_G # divide by RMSE
-	diff_W_stand = (obs_W - pred_W) / rmse_W # divide by RMSE
 
 	# Mask clouds, shadows, snow, nodata
 	diff_G[obs_mask > 0] = -9999
@@ -57,15 +48,9 @@ for img_id in img_list.index:
 	diff_G_stand[obs_mask > 0] = -9999
 	diff_G_stand[diff_G < -9999] = -9999
 
-	diff_W[obs_mask > 0] = -9999
-	diff_W[diff_W < -9999] = -9999
-
-	diff_W_stand[obs_mask > 0] = -9999
-	diff_W_stand[diff_G < -9999] = -9999
-
 	# Write out raw residuals: Greenness
 	in_ds = gdal.Open(img_obs_fn, gdal.GA_ReadOnly)
-	output_fn = '/projectnb/landsat/projects/Massachusetts/Moths/paper_data/int_products/{WRS2}_{date}_diff_G_0414.tif'.format(date=date, WRS2=WRS2)
+	output_fn = '../paper_data/int_products/{WRS2}_{date}_diff_G_0515.tif'.format(date=date, WRS2=WRS2)
 
 	out_driver = gdal.GetDriverByName("GTiff")
 	out_ds = out_driver.Create(output_fn, 
@@ -83,7 +68,7 @@ for img_id in img_list.index:
 
 	# Write out standardized residuals: Greenness
 	in_ds = gdal.Open(img_obs_fn, gdal.GA_ReadOnly)
-	output_fn = '/projectnb/landsat/projects/Massachusetts/Moths/paper_data/int_products/{WRS2}_{date}_diff_G_standardized_0414.tif'.format(date=date, WRS2=WRS2)
+	output_fn = '../paper_data/int_products/{WRS2}_{date}_diff_G_standardized_0515.tif'.format(date=date, WRS2=WRS2)
 
 	out_driver = gdal.GetDriverByName("GTiff")
 	out_ds = out_driver.Create(output_fn, 
@@ -97,40 +82,4 @@ for img_id in img_list.index:
 	out_ds.GetRasterBand(1).SetNoDataValue(-9999)
 	#out_ds.GetRasterBand(1).SetColorTable(ct)
 	out_ds.GetRasterBand(1).SetDescription('Standardized residuals: TCG')
-	out_ds = None
-
-	# Write out raw residuals: Wetness
-	in_ds = gdal.Open(img_obs_fn, gdal.GA_ReadOnly)
-	output_fn = '/projectnb/landsat/projects/Massachusetts/Moths/paper_data/int_products/{WRS2}_{date}_diff_W_0414.tif'.format(date=date, WRS2=WRS2)
-
-	out_driver = gdal.GetDriverByName("GTiff")
-	out_ds = out_driver.Create(output_fn, 
-	                           diff_W.shape[1],  # x size
-	                           diff_W.shape[0],  # y size
-	                           1,  # number of bands
-	                           gdal.GDT_Int16)
-	out_ds.SetProjection(in_ds.GetProjection())
-	out_ds.SetGeoTransform(in_ds.GetGeoTransform())
-	out_ds.GetRasterBand(1).WriteArray(diff_W)
-	out_ds.GetRasterBand(1).SetNoDataValue(-9999)
-	#out_ds.GetRasterBand(1).SetColorTable(ct)
-	out_ds.GetRasterBand(1).SetDescription('Raw residuals: TCW')
-	out_ds = None
-
-	# Write out standardized residuals: Wetness
-	in_ds = gdal.Open(img_obs_fn, gdal.GA_ReadOnly)
-	output_fn = '/projectnb/landsat/projects/Massachusetts/Moths/paper_data/int_products/{WRS2}_{date}_diff_W_standardized_0414.tif'.format(date=date, WRS2=WRS2)
-
-	out_driver = gdal.GetDriverByName("GTiff")
-	out_ds = out_driver.Create(output_fn, 
-	                           diff_W.shape[1],  # x size
-	                           diff_W.shape[0],  # y size
-	                           1,  # number of bands
-	                           gdal.GDT_Float32)
-	out_ds.SetProjection(in_ds.GetProjection())
-	out_ds.SetGeoTransform(in_ds.GetGeoTransform())
-	out_ds.GetRasterBand(1).WriteArray(diff_W)
-	out_ds.GetRasterBand(1).SetNoDataValue(-9999)
-	#out_ds.GetRasterBand(1).SetColorTable(ct)
-	out_ds.GetRasterBand(1).SetDescription('Standardized residuals: TCW')
 	out_ds = None
