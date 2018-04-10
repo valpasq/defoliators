efbbf1c2cb09280d26bdee25c0d62687ff44085a
Valerie <valpasq@bu.edu>
2018-04-10 13:38:36 -0400

    updated clip for all 5 MA P/Rs

--- code/3_cliptoWRS.sh
+++ code/3_cliptoWRS.sh
@@ -8,21 +8,21 @@
 # Adapted from: http://linfiniti.com/2009/09/clipping-rasters-with-gdal-using-polygons/
 
 #clean old clips
-rm ../2017_analysis/int_products/*WRS*
+rm ./int_products/*WRS*
 
-# p012r031
-SHPFILE1='../2017_analysis/p012r031_inputs/WRS2_p012r031_mod.shp'
+#### p011r031
+SHPFILE='/projectnb/landsat/projects/Massachusetts/defoliators/reanalysis/WRS2_bound/p011r031_WRS2_UTM19N_buffer.shp'
 
 # Calculate extent of shapefile (bounding box)
-BASE=`basename $SHPFILE1 .shp`
-EXTENT=`ogrinfo -so $SHPFILE1 $BASE | grep Extent \
+BASE=`basename $SHPFILE .shp`
+EXTENT=`ogrinfo -so $SHPFILE $BASE | grep Extent \
 | sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
 | sed 's/ - /, /g'`
 EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
 
-echo "Shapefile: $SHPFILE1"
+echo "Shapefile: $SHPFILE"
 
-r_file=$(find ../2017_analysis/int_products/ -name 'p012r031*standardized*.tif' -type f)
+r_file=$(find ./int_products/ -name 'p011r031*norm*.tif' -type f)
 
 for RASTERFILE in $r_file
 do
@@ -32,24 +32,24 @@ do
 	# Clip to polygon boundaries
 	gdalwarp -co COMPRESS=DEFLATE -co TILED=YES -of GTiff \
 	-r near -srcnodata -9999 -dstnodata -9999 \
-	-cutline $SHPFILE1 \
-	../2017_analysis/int_products/`basename $RASTERFILE .tif`.tif \
-	../2017_analysis/int_products/`basename $RASTERFILE .tif`_WRSclip.tif
+	-cutline $SHPFILE \
+	./int_products/`basename $RASTERFILE .tif`.tif \
+	./int_products/`basename $RASTERFILE .tif`_WRSclip.tif
 done
 
-# p013r031
-SHPFILE2='../2017_analysis/p013r031_inputs/WRS2_p013r031_mod.shp'
+#### p012r030
+SHPFILE='/projectnb/landsat/projects/Massachusetts/defoliators/reanalysis/WRS2_bound/p012r030_WRS2_UTM19N_buffer.shp'
 
 # Calculate extent of shapefile (bounding box)
-BASE=`basename $SHPFILE2 .shp`
-EXTENT=`ogrinfo -so $SHPFILE2 $BASE | grep Extent \
+BASE=`basename $SHPFILE .shp`
+EXTENT=`ogrinfo -so $SHPFILE $BASE | grep Extent \
 | sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
 | sed 's/ - /, /g'`
 EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
 
-echo "Shapefile: $SHPFILE2"
+echo "Shapefile: $SHPFILE"
 
-r_file=$(find ../2017_analysis/int_products/ -name 'p013r031*standardized*.tif' -type f)
+r_file=$(find ./int_products/ -name 'p012r030*norm*.tif' -type f)
 
 for RASTERFILE in $r_file
 do
@@ -59,9 +59,90 @@ do
 	# Clip to polygon boundaries
 	gdalwarp -co COMPRESS=DEFLATE -co TILED=YES -of GTiff \
 	-r near -srcnodata -9999 -dstnodata -9999 \
-	-cutline $SHPFILE2 \
-	../2017_analysis/int_products/`basename $RASTERFILE .tif`.tif \
-	../2017_analysis/int_products/`basename $RASTERFILE .tif`_WRSclip.tif
+	-cutline $SHPFILE \
+	./int_products/`basename $RASTERFILE .tif`.tif \
+	./int_products/`basename $RASTERFILE .tif`_WRSclip.tif
+done
+
+### p012r031
+SHPFILE='/projectnb/landsat/projects/Massachusetts/defoliators/reanalysis/WRS2_bound/p012r031_WRS2_UTM19N_buffer.shp'
+
+# Calculate extent of shapefile (bounding box)
+BASE=`basename $SHPFILE .shp`
+EXTENT=`ogrinfo -so $SHPFILE $BASE | grep Extent \
+| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
+| sed 's/ - /, /g'`
+EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
+
+echo "Shapefile: $SHPFILE"
+
+r_file=$(find ./int_products/ -name 'p012r031*norm*.tif' -type f)
+
+for RASTERFILE in $r_file
+do
+   echo "Running on file:"
+   echo $RASTERFILE
+
+	# Clip to polygon boundaries
+	gdalwarp -co COMPRESS=DEFLATE -co TILED=YES -of GTiff \
+	-r near -srcnodata -9999 -dstnodata -9999 \
+	-cutline $SHPFILE \
+	./int_products/`basename $RASTERFILE .tif`.tif \
+	./int_products/`basename $RASTERFILE .tif`_WRSclip.tif
+done
+
+### p013r030
+SHPFILE='/projectnb/landsat/projects/Massachusetts/defoliators/reanalysis/WRS2_bound/p013r030_WRS2_UTM18N_buffer.shp'
+
+# Calculate extent of shapefile (bounding box)
+BASE=`basename $SHPFILE .shp`
+EXTENT=`ogrinfo -so $SHPFILE $BASE | grep Extent \
+| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
+| sed 's/ - /, /g'`
+EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
+
+echo "Shapefile: $SHPFILE"
+
+r_file=$(find ./int_products/ -name 'p013r030*norm*.tif' -type f)
+
+for RASTERFILE in $r_file
+do
+   echo "Running on file:"
+   echo $RASTERFILE
+
+	# Clip to polygon boundaries
+	gdalwarp -co COMPRESS=DEFLATE -co TILED=YES -of GTiff \
+	-r near -srcnodata -9999 -dstnodata -9999 \
+	-cutline $SHPFILE \
+	./int_products/`basename $RASTERFILE .tif`.tif \
+	./int_products/`basename $RASTERFILE .tif`_WRSclip.tif
+done
+
+#### p013r031
+SHPFILE='/projectnb/landsat/projects/Massachusetts/defoliators/reanalysis/WRS2_bound/p013r031_WRS2_UTM18N_buffer.shp'
+
+# Calculate extent of shapefile (bounding box)
+BASE=`basename $SHPFILE .shp`
+EXTENT=`ogrinfo -so $SHPFILE $BASE | grep Extent \
+| sed 's/Extent: //g' | sed 's/(//g' | sed 's/)//g' \
+| sed 's/ - /, /g'`
+EXTENT=`echo $EXTENT | awk -F ',' '{print $1 " " $4 " " $3 " " $2}'`
+
+echo "Shapefile: $SHPFILE"
+
+r_file=$(find ./int_products/ -name 'p013r031*norm*.tif' -type f)
+
+for RASTERFILE in $r_file
+do
+   echo "Running on file:"
+   echo $RASTERFILE
+
+	# Clip to polygon boundaries
+	gdalwarp -co COMPRESS=DEFLATE -co TILED=YES -of GTiff \
+	-r near -srcnodata -9999 -dstnodata -9999 \
+	-cutline $SHPFILE \
+	./int_products/`basename $RASTERFILE .tif`.tif \
+	./int_products/`basename $RASTERFILE .tif`_WRSclip.tif
 done
 
 echo "Done!"
